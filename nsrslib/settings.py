d635def8406a298c543dbc1f72c9156ac3afdb42
s3h10r <s3h10r@gmail.com>
2014-10-04 13:59:45 +0200

    TODO mv bin-paths into settings

--- nsrslib/settings.py
+++ nsrslib/settings.py
@@ -2,3 +2,11 @@
 
 NSR_SERVER="localhost"
 
+# nsr-bins linux
+BIN_MMINFO="/usr/sbin/mminfo"
+BIN_NSRADMIN="/usr/sbin/nsradmin"
+# nsr-bins solaris 
+#BIN_MMINFO="/opt/nsr/mminfo"
+#BIN_NSRADMIN="/opt/nsr/nsradmin"
+
+
